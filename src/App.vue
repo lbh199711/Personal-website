64c66c612bbabffb87b10c2daab77334ad612542
Leo Liu <lbh199711@gmail.com>
2021-08-07 19:35:33 -0700

    GIT INIT

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,58 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+  
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import '@/assets/scss/_variables.scss';
+
+body {
+  letter-spacing: $letter-spacing-default;
+  font-size: $font-size-md;
+  font-family: $font-primary;
+  font-weight: 400;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+  background-color: $color-bg;
+  color: $color-black; 
+
+  margin: 0;
+  display: block;
+}
+
+a {
+  color: $color-link;
+  text-decoration: underline;
+
+  &:hover {
+    color: $color-link-highlight;
+  }
+}
+
+button, .button {
+  font-size: $font-size-md;
+
+  background-color: $color-button;
+  color: $color-button-text;
+
+  padding: .75rem 1.5rem;
+  border: none;
+  border-radius: 0.75rem;
+
+  &:hover {
+    background-color: $color-button-highlight;
+  }
 }
 
 #nav {
   padding: 30px;
+  background-color: $color-primary;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
